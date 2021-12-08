ba7eebfb67bd8678e38f085b515d10850752ebb3
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-08 08:57:23 +0530

    Initialized Firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+const config = {
+  apiKey: 'AIzaSyB1vG_dFSEgye03SsrAXLr8zTc-gXeBP2M',
+  authDomain: 'chat-web-app-ada7f.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-ada7f-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-ada7f',
+  storageBucket: 'chat-web-app-ada7f.appspot.com',
+  messagingSenderId: '335949561162',
+  appId: '1:335949561162:web:0986734859ae584920d5b3',
+};
+
+const app = firebase.initializeApp(config);
