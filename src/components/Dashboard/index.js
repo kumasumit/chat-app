11f27bf5428d31b6929e1a07101238af45a6584f
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-13 17:33:53 +0530

    completed uploading and displaying user-avatar

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -4,6 +4,7 @@ import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
 import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
+import AvatarUploadBtn from './AvatarUploadBtn';
 import ProviderBlock from './ProviderBlock';
 
 const DashBoard = ({ onSignOut }) => {
@@ -35,6 +36,7 @@ const DashBoard = ({ onSignOut }) => {
           onSave={onSave}
           label={<h6 className="mb-2">NickName</h6>}
         />
+        <AvatarUploadBtn />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -2,6 +2,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/storage';
 
 const config = {
   apiKey: 'AIzaSyB1vG_dFSEgye03SsrAXLr8zTc-gXeBP2M',
@@ -16,3 +17,4 @@ const config = {
 const app = firebase.initializeApp(config);
 export const auth = app.auth();
 export const database = app.database();
+export const storage = app.storage();
