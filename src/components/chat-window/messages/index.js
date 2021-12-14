a39ed08125557ac0a1953dc779e1aea2acfbfb60
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-14 13:50:50 +0530

    created layout for chat window

---
+++ src/components/chat-window/messages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Messages = () => {
+    return (
+        <div>
+            mesages
+        </div>
+    )
+}
+
+export default Messages
--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import { Loader } from 'rsuite';
+import Bottom from '../../components/chat-window/bottom';
+import Messages from '../../components/chat-window/messages';
+import Top from '../../components/chat-window/top';
+import { useRooms } from '../../context/rooms.context';
 
 const Chat = () => {
-  return <div>hello</div>;
+  const { chatId } = useParams();
+  const rooms = useRooms();
+  if (!rooms) {
+    return (
+      <Loader
+        center
+        vertical
+        size="md"
+        content="loading
+    "
+        speed="slow"
+      />
+    );
+  }
+  const currentRoom = rooms.find(room => room.id === chatId);
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page ">Chat {chatId} not found</h6>;
+  }
+  return (
+    <>
+      <div className="chat-top">
+        <Top />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <Bottom />
+      </div>
+    </>
+  );
 };
 
 export default Chat;
