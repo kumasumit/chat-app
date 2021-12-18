c197f3108f48b9dfa906ed5b887ef670e3ec3b3b
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-18 10:58:31 +0530

    added error boundary

--- src/App.js
+++ src/App.js
@@ -7,19 +7,22 @@ import { ProfileProvider } from './context/profile.context';
 import Home from './pages/Home';
 import SignIn from './pages/SignIn';
 import './styles/main.scss';
+import { ErrorBoundary } from './components/ErrorBoundary';
 
 function App() {
   return (
-    <ProfileProvider>
-      <Switch>
-        <PublicRoute path="/signin">
-          <SignIn />
-        </PublicRoute>
-        <PrivateRoute path="/">
-          <Home />
-        </PrivateRoute>
-      </Switch>
-    </ProfileProvider>
+    <ErrorBoundary>
+      <ProfileProvider>
+        <Switch>
+          <PublicRoute path="/signin">
+            <SignIn />
+          </PublicRoute>
+          <PrivateRoute path="/">
+            <Home />
+          </PrivateRoute>
+        </Switch>
+      </ProfileProvider>
+    </ErrorBoundary>
   );
 }
 
