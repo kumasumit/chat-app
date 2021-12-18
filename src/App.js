ecdbc0b2f9b5ebf1858a76b2c1bedfbfdd566c24
Kumar Sumit <learn.ksumit@gmail.com>
2021-12-18 11:04:21 +0530

    added lazy loading and Suspense

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Switch } from 'react-router-dom';
 import 'rsuite/dist/styles/rsuite-default.css';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import { ProfileProvider } from './context/profile.context';
 import Home from './pages/Home';
-import SignIn from './pages/SignIn';
 import './styles/main.scss';
 import { ErrorBoundary } from './components/ErrorBoundary';
+
+const SignIn = lazy(() => import('./pages/SignIn'));
 
 function App() {
   return (
@@ -15,7 +16,9 @@ function App() {
       <ProfileProvider>
         <Switch>
           <PublicRoute path="/signin">
-            <SignIn />
+            <Suspense fallback={<div>Loading...</div>}>
+              <SignIn />
+            </Suspense>
           </PublicRoute>
           <PrivateRoute path="/">
             <Home />
